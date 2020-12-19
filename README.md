1ee8b806a8d9b15b2429ffa8c020b741bb5d9e95
soohyeok <soohyeok0822@gmail.com>
2020-12-19 02:52:22 -0700

    init

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # sootube
 
 Cloning Youtube : NodeJS, vanillaJS, Express, Pug, MongoDB
+
+```bash
+npm install
+```
+
+```bash
+npm start
+```
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const express = require('express')
+import express from "express";
 const app = express()
  
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+const handleHome = (req, res) => {
+    res.send("hello world");
+}
+
+app.get("/", handleHome);
  
 app.listen(4000);
