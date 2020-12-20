738225bc217295ad6d6f4a1353940ea84b6c6bfa
soohyeok <soohyeok0822@gmail.com>
2020-12-19 20:48:39 -0700

    searchingBy Query

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,6 +1,10 @@
 export const home = (req, res) => res.render("home", {pageTitle: "Home"});
-export const search = (req, res) => res.render("search", {pageTitle: "Search"});
-
+export const search = (req, res) => {
+    const {
+        query: { term: searchingBy }
+    } = req;
+    res.render("search", {pageTitle: "Search", searchingBy});
+};
 export const videos = (req, res) => res.render("videos", {pageTitle: "Videos"});
 export const videoUpload = (req, res) => res.render("videoUpload", {pageTitle: "Video Upload"});
 export const videoDetail = (req, res) => res.render("videoDetail", {pageTitle: "Video Detail"});
