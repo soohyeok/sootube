a81688f752720b55efeea6aef155ad318fd32d74
soohyeok <soohyeok0822@gmail.com>
2020-12-20 01:21:10 -0700

    signup controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,23 @@
-export const signUp = (req, res) => res.render("signUp", {pageTitle: "signUp"});
+import routes from "../routes"
+
+export const getSignUp = (req, res) => {
+    res.render("signUp", {pageTitle: "signUp"});
+}
+export const postSignUp = (req, res) => {
+    const {
+        body: {name, email, password, password2}
+    } = req;
+    if(password !== password2){
+        res.status(406);
+        res.render("signUp", {pageTitle: "signUp"})
+    } else {
+        // To Do: Register User
+        // To Do: Log User In
+        res.redirect(routes.home)
+    }
+    
+}
+
 export const login = (req, res) => res.render("login", {pageTitle: "Login"});
 export const logout = (req, res) => res.render("logout", {pageTitle: "Logout"});
 
--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,13 +1,15 @@
 import express from "express";
-import { login, logout, signUp } from "../controllers/userController";
+import { login, logout, getSignUp, postSignUp} from "../controllers/userController";
 import { home, search } from "../controllers/videoController";
 import routes from "../routes";
 
 const globalRouter = express.Router();
 
+globalRouter.get(routes.signUp, getSignUp);
+globalRouter.post(routes.signUp, postSignUp);
+
 globalRouter.get(routes.home, home);
 globalRouter.get(routes.search, search);
-globalRouter.get(routes.signUp, signUp);
 globalRouter.get(routes.login, login);
 globalRouter.get(routes.logout, logout);
 
