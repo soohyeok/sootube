827b20499bc763da60b9404060e1ae8b81721704
soohyeok <soohyeok0822@gmail.com>
2020-12-19 22:28:15 -0700

    passing fake video data

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,4 +1,8 @@
-export const home = (req, res) => res.render("home", {pageTitle: "Home"});
+import {videosData} from "../db";
+
+export const home = (req, res) => {
+    res.render("home", {pageTitle: "Home", videosData})
+};
 export const search = (req, res) => {
     const {
         query: { term: searchingBy }
---
+++ db.js
@@ -0,0 +1,74 @@
+export const videosData = [
+    {
+        id: 353524,
+        title: 'Video Awesome',
+        description: 'sample video description',
+        views: 57,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 123123,
+            name: "Soohyeok",
+            email: "soohyeok@lee.com"
+        }
+    },
+    {
+        id: 65427,
+        title: 'Video 5455',
+        description: 'sample video description',
+        views: 272457,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 425724,
+            name: "Soohyeok",
+            email: "soohyeok@lee.com"
+        }
+    },
+    {
+        id: 26245,
+        title: 'Video 342523',
+        description: 'sample video description',
+        views: 246,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 26346,
+            name: "Soohyeok",
+            email: "soohyeok@lee.com"
+        }
+    },
+    {
+        id: 2364326,
+        title: 'Video 2352345',
+        description: 'sample video description',
+        views: 326,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 123627,
+            name: "Soohyeok",
+            email: "soohyeok@lee.com"
+        }
+    },
+    {
+        id: 9768,
+        title: 'Video 287594525',
+        description: 'sample video description',
+        views: 59679,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 6579569,
+            name: "Soohyeok",
+            email: "soohyeok@lee.com"
+        }
+    },
+    {
+        id: 6759,
+        title: 'Video 745815',
+        description: 'sample video description',
+        views: 6795,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 16956,
+            name: "Soohyeok",
+            email: "soohyeok@lee.com"
+        }
+    }
+]
