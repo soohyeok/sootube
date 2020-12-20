cf3d0b73df2cd7aa949f07daa870abe08e564ab1
soohyeok <soohyeok0822@gmail.com>
2020-12-20 05:17:21 -0700

    mongodb setup & dotenv

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,4 +1,3 @@
-import {videosData} from "../db";
 import routes from "../routes";
 
 export const home = (req, res) => {
--- db.js
+++ db.js
@@ -1,74 +1,19 @@
-export const videosData = [
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL,
     {
-        id: 353524,
-        title: 'Video Awesome',
-        description: 'sample video description 121',
-        views: 57,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 123123,
-            name: "Soohyeok",
-            email: "soohyeok@lee.com"
-        }
-    },
-    {
-        id: 65427,
-        title: 'Video 5455',
-        description: 'sample video description 424',
-        views: 272457,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 425724,
-            name: "Soohyeok",
-            email: "soohyeok@lee.com"
-        }
-    },
-    {
-        id: 26245,
-        title: 'Video 342523',
-        description: 'sample video description 4234',
-        views: 246,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 26346,
-            name: "Soohyeok",
-            email: "soohyeok@lee.com"
-        }
-    },
-    {
-        id: 2364326,
-        title: 'Video 2352345',
-        description: 'sample video description 23624',
-        views: 326,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 123627,
-            name: "Soohyeok",
-            email: "soohyeok@lee.com"
-        }
-    },
-    {
-        id: 9768,
-        title: 'Video 287594525',
-        description: 'sample video description 234',
-        views: 59679,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 6579569,
-            name: "Soohyeok",
-            email: "soohyeok@lee.com"
-        }
-    },
-    {
-        id: 6759,
-        title: 'Video 745815',
-        description: 'sample video description 25534',
-        views: 6795,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 16956,
-            name: "Soohyeok",
-            email: "soohyeok@lee.com"
-        }
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true
     }
-]
+);
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("Connected to db");
+const handleError = (error) => console.log(`Error on db: ${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
