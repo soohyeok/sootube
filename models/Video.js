61097fcc8ddbf269dd65acab15ee441179ef18b5
soohyeok <soohyeok0822@gmail.com>
2020-12-20 17:09:44 -0700

    model for comments

--- models/Video.js
+++ models/Video.js
@@ -19,8 +19,12 @@ const VideoSchema = new mongoose.Schema({
     createdAt: {
         type: Date,
         default: Date.now
-    }
-})
+    },
+    comments: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Comment"
+    }]
+});
 
 const model = mongoose.model("Video", VideoSchema);
 export default model;
