cbb34c35cf6f3ed9f28f81f7867c224830f9fe50
soohyeok <soohyeok0822@gmail.com>
2020-12-22 03:11:07 -0700

    header

--- app.js
+++ app.js
@@ -21,13 +21,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
-app.use(function contentSecurity(req, res, next) {
-  res.setHeader(
-    "Content-Security-Policy",
-    "default-src *; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://archive.org"
-  );
-  return next();
-});
+// app.use(function contentSecurity(req, res, next) {
+//   res.setHeader(
+//     "Content-Security-Policy",
+//     "script-src 'self' https://archive.org"
+//   );
+//   return next();
+// });
 
 app.use(localsMiddleware);
 
