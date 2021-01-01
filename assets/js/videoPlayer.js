82fe93ce8700e1044a6b6dec10a90e0325d36d85
soohyeok <soohyeok0822@gmail.com>
2021-01-01 15:20:52 -0500

    video player toFullscreen,exitfullscreen

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -2,6 +2,7 @@ const videoContainer = document.getElementById("jsVideoPlayer");
 const videoPlayer = document.querySelector("#jsVideoPlayer video");
 const playBtn = document.getElementById("jsPlayButton");
 const volumeBtn = document.getElementById("jsVolumeButton");
+const fullScreenBtn = document.getElementById("jsFullScreen");
 
 function handlePlayClick() {
   if (videoPlayer.paused) {
@@ -23,9 +24,24 @@ function handleVolumeClick() {
   }
 }
 
+function exitFullScreen() {
+  document.webkitExitFullscreen();
+  fullScreenBtn.innerHTML = '<i class="fas fa-expand"></i>';
+  fullScreenBtn.removeEventListener("click", exitFullScreen);
+  fullScreenBtn.addEventListener("click", toFullScreen);
+}
+
+function toFullScreen() {
+  videoContainer.webkitRequestFullScreen();
+  fullScreenBtn.innerHTML = '<i class="fas fa-compress"></i>';
+  fullScreenBtn.removeEventListener("click", toFullScreen);
+  fullScreenBtn.addEventListener("click", exitFullScreen);
+}
+
 function init() {
   playBtn.addEventListener("click", handlePlayClick);
   volumeBtn.addEventListener("click", handleVolumeClick);
+  fullScreenBtn.addEventListener("click", toFullScreen);
 }
 
 if (videoContainer) {
